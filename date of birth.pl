c4c76c7f7b145f57a005534735aae250c25c0160
kramya1106 <113620880+kramya1106@users.noreply.github.com>
2023-02-14 22:36:41 +0530

    Create date of birth.pl

---
+++ date of birth.pl
@@ -0,0 +1,9 @@
+% Define the born/4 predicate, which states that the first argument is a person with the second argument as their day of birth, the third argument as their month of birth, and the fourth argument as their year of birth.
+born(jan, 20, 3, 1977).
+born(jeroen, 2, 2, 1992).
+born(joris, 17, 3, 1995).
+born(jelle, 1, 1, 2004).
+born(jesus, 24, 12, 2000).
+born(joop, 30, 4, 1989).
+born(jannecke, 17, 3, 1993).
+born(jaap, 16, 11, 1995).
